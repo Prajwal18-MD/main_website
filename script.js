ec7d78542b15a183a44ec6fac7db012e644a70db
Prajwal-18MD <prajju.18gryphon@gmail.com>
2024-05-20 16:46:07 +0530

    script

--- script.js
+++ script.js
@@ -58,26 +58,21 @@ var mixer = mixitup('.portfolio-gallery',{
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
     spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
     },
-    autoplay:{
-        delay:3000,
-        disableOnInteraction:false,
-    },
-
     breakpoints: {
-        576:{
-            slidesPerView:2,
-            spaceBetween:10,
+        576: {
+            slidesPerView: 2,
+            spaceBetween: 10,
         },
-        1200:{
-            slidesPerView:3,
-            spaceBetween:20,
+        1200: {
+            slidesPerView: 3,
+            spaceBetween: 20,
         },
     }
-  });
+});
 
 
 
